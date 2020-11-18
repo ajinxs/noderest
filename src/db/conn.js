df59bd01ede25b853ae4ed198fff75fa0933a712
Ajinkya <leeajink@gmail.com>
2020-11-18 22:44:14 +0700

    changes in db

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose")
 
-//mongodb://localhost:27017/schoodata
-mongoose.connect("mongodb://localhost:27017/schooldata",{
+mongoose.connect("",{
     useCreateIndex:true,
     useNewUrlParser:true,
     useUnifiedTopology:true
